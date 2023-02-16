ed485f4bde162f732cba449b7809a402f8a9f521
GitHub Action <action@github.com>
2023-02-16 00:12:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/linwancen/drawgraph/MyBundle.kt
+++ src/main/kotlin/com/github/linwancen/drawgraph/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.linwancen.drawgraph
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/linwancen/drawgraph/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/linwancen/drawgraph/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.linwancen.drawgraph.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.linwancen.drawgraph.MyBundle
 
 class MyApplicationService {
 
